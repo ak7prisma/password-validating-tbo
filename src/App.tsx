c81c0ac02e641bf65fd5e59299f4ab29c4bbcadf
AKzPrisma <ahmadkurniaprisma@gmail.com>
2026-04-08 13:53:35 +0700

    feat: add structure website

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Footer from './components/Footer'
+import MainSection from './components/Mainsection'
 
 export default function App() {
 
   return (
     <>
-      
+      <MainSection />
+      <Footer />
     </>
   )
 }
---
+++ src/components/molecule/ResultContent.tsx
@@ -0,0 +1,8 @@
+export default function ResulContent() {
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
